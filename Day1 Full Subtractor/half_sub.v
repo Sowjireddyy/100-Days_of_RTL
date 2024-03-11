ba91bf03317f862f032ef5abe351fc10db149374
sowjanya kumar reddy Bommaka <135091527+Sowjireddyy@users.noreply.github.com>
2024-03-11 22:51:23 +0530

    Add files via upload

---
+++ Day1 Full Subtractor/half_sub.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03.03.2024 18:54:39
+// Design Name: 
+// Module Name: half_sub
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module half_sub(
+    input a,
+    input b,
+    output diff,
+    output bor
+    );
+    assign diff=a^b;
+    assign bor=~a&b;
+endmodule
