eeb456822f717aa981b96bc93a7d0160d0fd7109
sowjanya kumar reddy Bommaka <135091527+Sowjireddyy@users.noreply.github.com>
2024-06-20 09:44:36 +0530

    Add files via upload

---
+++ Day_81_Fork_Join_any/top.sv
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 20.06.2024 09:29:39
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+module top();
+initial
+  begin
+    #10;
+    fork
+      begin
+       #20
+       $display("thread -1 is displayed at time T=%0t",$time);
+      end
+      begin:b
+      #40;
+       $display("thread -2 is displayed at time T=%0t",$time);
+      end
+      begin
+       #30;
+        $display("thread -3 is displayed at time T=%0t",$time);
+      end
+      join_any
+       $display("exit is at time T=%0t",$time);
+     end
+endmodule
